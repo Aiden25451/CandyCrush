cec4f98297deaf4f935a761b596eccf902f459ce
Aiden <a.asprakis@gmail.com>
2024-03-05 15:51:52 -0500

    Second Commit

--- src/com/candycrush/main/Launcher.java
+++ src/com/candycrush/main/Launcher.java
@@ -2,7 +2,7 @@ package com.candycrush.main;
 
 public class Launcher {
 	public static void main(String[] args) {
-		System.out.println("HELLOWORLD");
+		System.out.println("HELLOWORLD1");
 	}
 	
 }
